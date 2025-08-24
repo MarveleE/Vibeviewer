a198f647ecd23f09cb84c7bef9ded6a0bcb28961
Groot <cn173619601@gmail.com>
2025-08-24 19:53:33 +0800

    Initial Commit

---
+++ Vibeviewer/VibeviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeviewerApp.swift
+//  Vibeviewer
+//
+//  Created by Groot chen on 2025/8/24.
+//
+
+import SwiftUI
+
+@main
+struct VibeviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
