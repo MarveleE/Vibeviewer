5afac1239ec91be07a1c6cb79fd8a7830c265957
Groot <cn173619601@gmail.com>
2025-11-09 18:09:27 +0800

    chore: 更新版本号至 1.1.6 并更新 appcast.xml

--- Project.swift
+++ Project.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 let workspaceName = "Vibeviewer"
 
 // 版本号统一配置 - 只在这里修改版本号
-let appVersion = "1.1.8"
+let appVersion = "1.1.6"
 
 let project = Project(
     name: workspaceName,
